c09e94676cf2ce63d35b7e20e0eee78b57f5749b
kaushal8778 <kaushalrupapara2125@gmial.com>
2025-04-30 13:33:46 +0530

    read_webcam_added

---
+++ Section #1 - Basics/read_webcam.py
@@ -0,0 +1,10 @@
+import cv2
+capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = capture.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(40) & 0xFF == ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
